a570814b345d1cfd56cb9b8044eb05635cf135cf
BigEd33 <big.eddy.ee@gmail.com>
2023-10-06 02:10:42 +0300

    v1.0

--- test/client.test.js
+++ test/client.test.js
@@ -1,15 +1,15 @@
-import DiscordDB from "../src/main.mjs";
+import DiscordDB from "../src/main.js";
 import assert from "assert";
 
 describe("DiscordDB Client", () => {
 	it("should reject empty values", () => {
 		assert.throws(() => {
-			new DiscordDB({ token: "testing", guildId: "testing" });
+			new DiscordDB({ guildId: "testing" });
 		}, { name: "DiscordDBError", message: "Missing required fields." });
 	});
 
 	it("should reject an invalid token", async () => {
-		const client = new DiscordDB({ token: "testing", guildId: "testing", categoryId: "testing" });
+		const client = new DiscordDB({ token: "testing", guildId: "testing" });
 		try {
 			await client.login();
 			return assert.fail("Verification passed when it should have failed");
